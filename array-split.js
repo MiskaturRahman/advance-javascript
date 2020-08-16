5789af1be27b94cbe68921be50fccb0572599558
Misk007 <mdrahmanmiskatur@gmail.com>
2020-08-16 11:31:33 +0600

    update

--- array-split.js
+++ array-split.js
@@ -1,6 +1,11 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8];
-const part = nums.slice(2, 5);//picks up selected items but main array remains fine
+const part = nums.slice(2, 4);//picks up selected index start to stop items but main array remains fine
 
-const removed = nums.splice(2, 4, 55, 54);//changes the main array.. we can a 
+const removed = nums.splice(2, 4, 55, 54);//changes the main array., remove from starting index to desired numbers
+
 console.log(removed);
+// console.log(part);
 console.log(nums);
+
+const together = nums.join(" hello ");
+console.log(together);
